b13d8f1a38e2c866471445daf15bb85182b81b5f
AlexandrAnatoliev <per-1986@list.ru>
2022-10-29 16:00:29 +0300

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+# freelanceTask4
+
+# Описание
+#
+# Написание лабораторных работ
+# Необходимо написать 2 лабораторные работы
+# 1. Реализовать сложение, умножение, транспонирование и умножение на число матриц (ВАЖНО: без использования numpy)
+# 2. Реализовать решение системы линейных уравнений (СЛАУ)
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
