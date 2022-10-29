a7647a6415614f8f5b2f114af6a85174e7c00329
AlexandrAnatoliev <per-1986@list.ru>
2022-10-29 16:48:37 +0300

    Ввод данных в матрицу1

--- main.py
+++ main.py
@@ -13,6 +13,23 @@ print("""Введите первую матрицу в формате:
     7 8 9
     """)
 
+# Создание квадратной матрицы:
+# 1 0 1
+# 0 1 1 => [[1,0,1],[0,1,1],[1,1,1]]
+# 1 1 1
+
+
+string1 = list(map(float, input().split()))  # считываем первую строку матрицы
+matrix_size = len(string1)  # вычисляем размер матрицы
+matrix1 = []
+matrix1.append(string1)
+# Создаем матрицу
+for i in range(matrix_size - 1):
+    string1 = list(map(float, input().split()))
+    matrix1.append(string1)
+
+print(string1)
+print(matrix1)
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
